be2295a459473c06b372a0861ef0ba8de4e8623e
Paul Meng <mno2@mno2.org>
2016-06-19 22:24:03 +0800

    Update Build Script and Directory

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-pandoc --standalone --section-divs \
-  --variable theme="white" \
-  --variable transition="fade" \
-  -s -f markdown -t revealjs \
-  neovim-plugin-in-haskell.md -o neovim-plugin-in-haskell.html 
+SRC_DIR=src
+OUT_DIR=derived
+
+for e in $(ls $SRC_DIR/*.md)
+do
+  f=$(basename $e)
+  filename="${f%.*}"
+  pandoc --standalone --section-divs \
+    --variable theme="white" \
+    --variable transition="fade" \
+    -s -f markdown -t revealjs \
+    ${SRC_DIR}/${filename}.md -o ${OUT_DIR}/${filename}.html
+done
